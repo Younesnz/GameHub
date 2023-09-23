cf1905d81cdfac2de23f44f2ad04cee5cf889a30
Younes Nourzehi <unesnz@gmail.com>
2023-09-22 21:48:48 -0700

    add color to theme icons

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -5,9 +5,18 @@ export const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack>
-      <SunIcon boxSize="1rem" />
-      <Switch isChecked={colorMode === "dark"} onChange={toggleColorMode} />
-      <MoonIcon boxSize="1rem" />
+      <SunIcon boxSize="1rem" color="orange.300" />
+      <Switch
+        isChecked={colorMode === "dark"}
+        onChange={toggleColorMode}
+        colorScheme="purple"
+        sx={{
+          "span.chakra-switch__track:not([data-checked])": {
+            backgroundColor: "orange.300",
+          },
+        }}
+      />
+      <MoonIcon boxSize="1rem" color="purple.300" />
     </HStack>
   );
 };
