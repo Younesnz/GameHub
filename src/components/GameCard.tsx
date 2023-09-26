03b52e8928974fee6f3e30001fc925b03725eb09
Younes Nourzehi <unesnz@gmail.com>
2023-09-25 22:11:30 -0700

    Optimize Images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,6 +9,7 @@ import {
 import { Game } from "../hooks/useGames";
 import { PlatformList } from "./PlatformList";
 import { CriticScore } from "./CriticScore";
+import cropImgUrl from "../services/cropImgUrl";
 
 interface Props {
   game: Game;
@@ -18,7 +19,7 @@ export const GameCard = ({ game }: Props) => {
   return (
     <Card overflow="hidden">
       <Image
-        src={game.background_image}
+        src={cropImgUrl(game.background_image)}
         h={{ sm: "64", md: "52", lg: "48", xl: "40" }}
         objectFit="cover"
       />
