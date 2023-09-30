e6750248e87f43c64edd18e6b87a089ab66c3410
Younes Nourzehi <unesnz@gmail.com>
2023-09-29 20:44:53 -0700

    Fetch the Genres

---
+++ src/components/Genres.tsx
@@ -0,0 +1,25 @@
+import { List, ListItem, useToast } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+
+export const Genres = () => {
+  const { genres, isLoading, error } = useGenres();
+  const toast = useToast();
+  if (error)
+    toast({
+      title: "Getting the Genres List Failed!",
+      description: error,
+      duration: 5000,
+      isClosable: true,
+      status: "error",
+      position: "top",
+    });
+  return (
+    <List>
+      {genres.map((genre) => (
+        <ListItem>
+          {genre.name} ({Math.round(genre.games_count / 1000)}k)
+        </ListItem>
+      ))}
+    </List>
+  );
+};
